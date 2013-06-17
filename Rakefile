fe0609af6ae44a61b27018832d7fdaab648b09dd
Vinylfabrikken <spacetravel.no@gmail.com>
2013-06-17 17:22:02 +0200

    Initial commit

---
+++ Rakefile
@@ -0,0 +1,11 @@
+# Add your own tasks in files placed in lib/tasks ending in .rake,
+# for example lib/tasks/capistrano.rake, and they will automatically be available to Rake.
+
+require File.expand_path('../config/application', __FILE__)
+
+FirstApp::Application.load_tasks
+
+# Rakefile
+task :freedom do
+  Bundler.with_clean_env { sh "heroku" }
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = 'ce099c1c272bec31e9247638d23073622f17277985e3ff10dbbb50f8f88b25a224d488f3c0b2bf0cd13083d98843430b46d03e7c974e34b9fbc2cecc429b12d1'
